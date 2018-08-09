c4c4e3febb48a4b92497503d5d5cfe9a38d702fe
joeljoeljoel <joel@viastudio.com>
2018-08-09 10:08:18 -0400

    Finished Video 3

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return <p>
+            Hello StorePicker!
+        </p>;
+    }
+}
+
+export default StorePicker;
